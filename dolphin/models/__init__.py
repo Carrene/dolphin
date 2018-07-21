0f3cdc96c3216863640792ae6dea7219969ef6b9
shayan <shayan>
2018-07-21 13:40:03 +0430

    Add tag model

--- dolphin/models/__init__.py
+++ dolphin/models/__init__.py
@@ -3,10 +3,9 @@ from .stakeholder import Stakeholder
 from .subscribable import Subscribable
 from .project import Project
 from .release import Release
-from .task import Task
+from .task import Task, Tag
 # from .admin import Admin
 # from .stage import Stage
-# from .tag import Tag
 # from .resource import Resource
 # from .guest import Guest
 # from .team import Team
--- dolphin/models/task.py
+++ dolphin/models/task.py
@@ -1,9 +1,17 @@
 
-from sqlalchemy import Integer, Time, ForeignKey, DateTime, Enum
-from restfulpy.orm import Field
-from restfulpy.orm.mixins import TimestampMixin
+from sqlalchemy import Integer, Time, ForeignKey, DateTime, Enum, String
+from restfulpy.orm import Field, DeclarativeBase, TimestampMixin, relationship
 
 from .subscribable import Subscribable
+
+
+class Tag(DeclarativeBase):
+    __tablename__ = 'tag'
+
+    id = Field(Integer, primary_key=True)
+    name = Field(String, max_length=40, unique=True, example='feature')
+
+    tasks = relationship('Task', backref='tag')
 
 
 class Task(Subscribable):
@@ -11,7 +19,7 @@ class Task(Subscribable):
     __mapper_args__ = {'polymorphic_identity': __tablename__}
 
     id = Field(Integer, ForeignKey('subscribable.id'), primary_key=True)
-#    tag_id = Field(Integer, ForeignKey('tag.id'))
+    tag_id = Field(Integer, ForeignKey('tag.id'))
 #    stage_id = Field(Integer, ForeignKey('stage.id'))
     kind = Field(
         Enum('feature', 'enhancement', 'bug', name='kind'),
