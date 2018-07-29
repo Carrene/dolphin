08c0a57c97c492cbd5a13ce3fa9854a992d13193
shayan <shayan>
2018-07-29 13:46:17 +0430

    Replace long invalid string

--- dolphin/tests/test_project.py
+++ dolphin/tests/test_project.py
@@ -6,19 +6,6 @@ from dolphin.controllers.root import Root
 from dolphin.models import Project, Administrator, Release
 
 
-long_invalid_string = '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo \
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse \
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Sed \
-ut perspiciatis unde omnis iste natus error sit voluptatem accusantium \
-doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore \
-veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-'''
-
-
 class TestProject(LocalApplicationTestCase):
     __controller_factory__ = Root
 
@@ -94,7 +81,7 @@ class TestProject(LocalApplicationTestCase):
 
             when(
                 'Description length is less than limit',
-                form=Update(description=long_invalid_string)
+                form=Update(description=(515*'a'))
             )
             assert status == '703 At most 512 characters are valid for '\
                 'description'
