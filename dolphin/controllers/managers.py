4563c17d8e02620c08eb23f5207108e822f3672e
shayan-7 <39003982+shayan-7@users.noreply.github.com>
2018-08-04 13:53:05 +0430

    Add assign verb to manager, closes #40 (#48)
    * Add assign verb to manager, #40
    
    * Implement all tests, closes #40
    
    * Enhance coding style, closes #40
    
    * Enhance test descriptions, closes #40

---
+++ dolphin/controllers/managers.py
@@ -0,0 +1,40 @@
+from nanohttp import HTTPStatus, json, context, HTTPNotFound, HTTPBadRequest
+from restfulpy.orm import DBSession, commit
+from restfulpy.controllers import ModelRestController
+
+from dolphin.models import Manager, Project
+from dolphin.validators import assign_manager_validator
+
+
+class ManagerController(ModelRestController):
+    __model__ = Manager
+
+    @json
+    @assign_manager_validator
+    @Project.expose
+    @commit
+    def assign(self, id):
+        form = context.form
+
+        # FIXME: This validation must be performed inside the validation
+        # decorator
+        try:
+            id = int(id)
+        except:
+            raise HTTPNotFound()
+
+        manager = DBSession.query(Manager) \
+            .filter(Manager.id == id).one_or_none()
+        if not manager:
+            raise HTTPNotFound()
+
+        project = DBSession.query(Project) \
+            .filter(Project.id == form['projectId']).one_or_none()
+        if not project:
+            raise HTTPStatus(f'601 Project not found with id: '
+                             f'{form["projectId"]}'
+            )
+
+        project.manager = manager
+        return manager
+
---
+++ dolphin/tests/test_manager.py
@@ -0,0 +1,89 @@
+
+from bddrest import status, response, Update, when, Remove, Append
+
+from dolphin.tests.helpers import LocalApplicationTestCase
+from dolphin.models import Project, Manager, Release
+
+
+class TestManager(LocalApplicationTestCase):
+
+    @classmethod
+    def mockup(cls):
+        session = cls.create_session()
+
+        assigned_manager = Manager(
+            title='First Manager',
+            email=None,
+            phone=123456789
+        )
+        session.add(assigned_manager)
+        session.flush()
+
+        unassigned_manager = Manager(
+            title='Second Manager',
+            email=None,
+            phone=987654321
+        )
+        session.add(unassigned_manager)
+        session.flush()
+
+        release = Release(
+            manager=assigned_manager,
+            title='My first release',
+            description='A decription for my release',
+            due_date='2020-2-20',
+            cutoff='2030-2-20',
+        )
+        session.add(release)
+        session.flush()
+
+        project = Project(
+            manager=assigned_manager,
+            releases=release,
+            title='My first project',
+            description='A decription for my project',
+            due_date='2020-2-20',
+        )
+        session.add(project)
+        session.commit()
+
+    def test_assign(self):
+        with self.given(
+            'Assign a manager to project',
+            '/apiv1/managers/id:2',
+            'ASSIGN',
+            form=dict(projectId='2')
+        ):
+            assert status == 200
+
+            when(
+                'Intended manager with string type not found',
+                url_parameters=dict(id='Alphabetical')
+            )
+            assert status == 404
+
+            when(
+                'Intended manager with integer type not found',
+                url_parameters=dict(id=100)
+            )
+            assert status == 404
+
+            when(
+                'Project id is not in form',
+                form=Remove('projectId')
+            )
+            assert status == '713 Project id not in form'
+
+            when(
+                'Project id type is not integer',
+                form=Update(projectId='Alphabetical')
+            )
+            assert status == '714 Invalid project id type'
+
+            when(
+                'Project not found',
+                form=dict(projectId=100)
+            )
+            assert status == 601
+            assert status.text.startswith('Project not found ')
+
