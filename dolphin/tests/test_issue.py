da9917e1c74c41bb4ca138946fc3c8e45bebb8d0
shayan-7 <39003982+shayan-7@users.noreply.github.com>
2018-08-06 19:22:39 +0430

    Add update verb to issue, closes #55 (#61)
    * Establish relationship between item and stage, closes #59
    
    * Add update verb to issue, closes #55

--- dolphin/tests/test_issue.py
+++ dolphin/tests/test_issue.py
@@ -76,6 +76,57 @@ class TestIssue(LocalApplicationTestCase):
             assert response.json['status'] == 'triage'
 
             when(
+                'Title is not in form',
+                form=Remove('title')
+            )
+            assert status == '710 Title not in form'
+
+            when(
+                'Title length is more than limit',
+                form=Update(
+                    title='This is a title with the length more than 50 '\
+                    'characters'
+                )
+            )
+            assert status == '704 At most 50 characters are valid for title'
+
+            when(
+                'Description length is less than limit',
+                form=Update(description=((512 + 1) * 'a'))
+            )
+            assert status == '703 At most 512 characters are valid for '\
+                'description'
+
+            when(
+                'Due date format is wrong',
+                form=Update(dueDate='20-20-20')
+            )
+            assert status == '701 Invalid due date format'
+
+            when(
+                'Due date is not in form',
+                form=Remove('dueDate')
+            )
+            assert status == '711 Due date not in form'
+
+            when(
+                'Kind is not in form',
+                form=Remove('kind')
+            )
+            assert status == '718 Kind not in form'
+
+            when(
+                'Days is not in form',
+                form=Remove('days')
+            )
+            assert status == '720 Days not in form'
+
+            when(
+                'Days type is wrong',
+                form=Update(days='Alphabetical')
+            )
+            assert status == '721 Invalid days type'
+            when(
                 'Title is repetitive',
                 form=Update(title='First issue')
             )
@@ -95,4 +146,95 @@ class TestIssue(LocalApplicationTestCase):
             )
             assert status == 705
             assert status.text.startswith('Invalid status')
+
+    def test_update(self):
+        with self.given(
+            'Update a issue',
+            '/apiv1/issues/id:3',
+            'UPDATE',
+            form=dict(
+                title='New issue',
+                description='This is a description for new issue',
+                dueDate='2200-12-12',
+                kind='feature',
+                days=4,
+            )
+        ):
+            assert status == 200
+
+            when(
+                'Intended issue with string type not found',
+                url_parameters=dict(id='Alphabetical')
+            )
+            assert status == 404
+
+            when(
+                'Intended issue with string type not found',
+                url_parameters=dict(id=100)
+            )
+            assert status == 404
+
+            when(
+                'Title length is more than limit',
+                form=Update(
+                    title='This is a title with the length more than 50 '\
+                    'characters'
+                )
+            )
+            assert status == '704 At most 50 characters are valid for title'
+
+            when(
+                'Description length is less than limit',
+                form=Update(description=((512 + 1) * 'a'))
+            )
+            assert status == '703 At most 512 characters are valid for '\
+                'description'
+
+            when(
+                'Due date format is wrong',
+                form=Update(dueDate='20-20-20')
+            )
+            assert status == '701 Invalid due date format'
+
+            when(
+                'Intended issue with string type not found',
+                url_parameters=dict(id='Alphabetical')
+            )
+            assert status == 404
+
+            when(
+                'Invalid kind value is in form',
+                form=Update(kind='enhancing')
+            )
+            assert status == 717
+            assert status.text.startswith('Invalid kind')
+
+            when(
+                'Invalid status value is in form',
+                form=Append(status='progressing')
+            )
+            assert status == 705
+            assert status.text.startswith('Invalid status')
+
+            when(
+                'Title is repetitive',
+                form=Update(title='Defined issue')
+            )
+            assert status == 600
+            assert status.text.startswith('Another project with title')
+
+            when(
+                'Invalid parameter is in the form',
+                form=Append(invalid_param='This is an external parameter')
+            )
+            assert status == 707
+            assert status.text.startswith('Invalid field')
+
+        with self.given(
+            'Updating project with empty form',
+            '/apiv1/projects/id:2',
+            'UPDATE',
+            form=dict()
+        ):
+            assert status == '708 No parameter exists in the form'
 
