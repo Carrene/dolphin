4ad28d2280a498572a3ca509408a4c91a651bb8c
shayan <shayan>
2018-08-08 13:34:08 +0430

    Migrate to new nanohttp validators

--- dolphin/validators.py
+++ dolphin/validators.py
@@ -1,14 +1,26 @@
 import re
 
 from nanohttp import validate, HTTPStatus, context
-from restfulpy.orm import DBSession
+from restfulpy.orm import DBSession, commit
 
+from dolphin.models import Project
 from dolphin.exceptions import empty_form_http_exception
 
 
 DATE_PATTERN = re.compile(
     '^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])$'
 )
+
+
+def project_not_exists_validator(title, container, field):
+
+    project = DBSession.query(Project).filter(Project.title == title) \
+        .one_or_none()
+    if project is not None:
+        raise HTTPStatus(
+            f'600 A project with title: {title} is already exists.'
+        )
+    return title
 
 
 release_validator = validate(
@@ -49,7 +61,7 @@ update_release_validator = validate(
 project_validator = validate(
     title=dict(
         required=('710 Title not in form'),
-        not_none=('727 Ttile is null'),
+        callback=(project_not_exists_validator),
         max_length=(50, '704 At most 50 characters are valid for title')
     ),
     description=dict(
