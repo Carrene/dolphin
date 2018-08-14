43a538ba40787f65cff8a87a2935582cca6e4dab
shayan-7 <39003982+shayan-7@users.noreply.github.com>
2018-08-14 15:24:44 +0430

    Add verb unsubscribe to issue, closes #10 (#74)
    * Remove url parameter id validator
    
    * Add verb subscribe to issue, closes #9
    
    * Add verb unsubscribe issue, closes #10
    
    * Ehance namings, closes #10

--- dolphin/controllers/issues.py
+++ dolphin/controllers/issues.py
@@ -1,3 +1,4 @@
+
 from nanohttp import HTTPStatus, json, context, HTTPNotFound
 from restfulpy.utils import to_camel_case
 from restfulpy.orm import DBSession, commit
@@ -86,5 +87,33 @@ class IssueController(ModelRestController):
             member=form['memberId']
         )
         DBSession.add(subscription)
+
+        return issue
+
+    @json
+    @subscribe_issue_validator
+    @Issue.expose
+    @commit
+    def unsubscribe(self, id):
+        form = context.form
+
+        try:
+            id = int(id)
+        except:
+            raise HTTPNotFound()
+
+        issue = DBSession.query(Issue).filter(Issue.id == id).one_or_none()
+        if not issue:
+            raise HTTPNotFound()
+
+        subscription = DBSession.query(Subscription).filter(
+            Subscription.subscribable == id,
+            Subscription.member == form['memberId']
+        ).one_or_none()
+
+        if not subscription:
+            raise HTTPStatus('612 Not Subscribed Yet')
+
+        DBSession.delete(subscription)
         return issue
 
