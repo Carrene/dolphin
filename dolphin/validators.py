42fcda4067dfc53cd3ce5ef64e6386f49da7b5ce
pylover <vahid.mardani@gmail.com>
2018-09-09 16:49:55 +0430

    Fixing the invalid form parameter! Release instance!

--- dolphin/validators.py
+++ dolphin/validators.py
@@ -10,7 +10,7 @@ from dolphin.exceptions import empty_form_http_exception
 
 
 DATE_PATTERN = re.compile(
-    '^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])$'
+    r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])$'
 )
 
 
