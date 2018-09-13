1b88cba02b0e1d80c869410902d9ead588a0bcc2
shayan <shayan>
2018-09-13 15:20:14 +0430

    Enhance token controller

--- dolphin/controllers/oauth2.py
+++ dolphin/controllers/oauth2.py
@@ -1,5 +1,4 @@
 from restfulpy.controllers import RestController
-<<<<<<< HEAD
 
 from .tokens import TokenController
 
@@ -10,15 +9,4 @@ class OAUTHController(RestController):
     def __call__(self, *remaining_paths):
         if len(remaining_paths) == 1 and remaining_paths[0] == 'tokens':
             return self._token_controller()
-=======
-from .token import TokenController
-
-
-class OAUTHController(RestController):
-    __token_controller = TokenController()
-
-    def __call__(self, *remaining_paths):
-        if len(remaining_paths) == 1  and remaining_paths[0] == 'tokens':
-            return self.__token_controller()
->>>>>>> Enhance integration
 
