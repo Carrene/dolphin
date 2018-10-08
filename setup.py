71835049edf64439b6dcf29ffa032555dbd0d984
shayan <shayan>
2018-10-08 12:47:33 +0330

    Migrate from restfulpy 2.3 to 2.3.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(join(dirname(__file__), 'dolphin', '__init__.py')) as v_file:
 
 
 dependencies = [
-    'restfulpy >= 2.3',
+    'restfulpy >= 2.3.1',
 #    'cas-common',
 
     # Deployment
