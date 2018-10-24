308de12fdb3ad3c1f81615a01cc0ea131bb6c763
shayan <shayan>
2018-10-24 17:39:00 +0330

    Enhance test_metadata_issue

---
+++ dolphin/tests/test_metadata_issue.py
@@ -0,0 +1,62 @@
+from bddrest.authoring import status, response
+
+from dolphin.tests.helpers import LocalApplicationTestCase
+
+
+class TestIssue(LocalApplicationTestCase):
+
+    def test_metadata(self):
+        with self.given(
+            'Test metadata verb',
+            '/apiv1/issues',
+            'METADATA'
+        ):
+            fields = response.json['fields']
+
+            assert status == 200
+
+            assert fields['title']['maxLength'] is not None
+            assert fields['title']['minLength'] is not None
+            assert fields['title']['label'] is not None
+            assert fields['title']['watermark'] is not None
+            assert fields['title']['pattern'] is not None
+            assert fields['title']['example'] is not None
+            assert fields['title']['name'] is not None
+            assert fields['title']['not_none'] is not None
+            assert fields['title']['required'] is not None
+
+            assert fields['description']['maxLength'] is not None
+            assert fields['description']['minLength'] is not None
+            assert fields['description']['label'] is not None
+            assert fields['description']['watermark'] is not None
+            assert fields['description']['name'] is not None
+            assert fields['description']['not_none'] is not None
+            assert fields['description']['required'] is not None
+
+            assert fields['status']['label'] is not None
+            assert fields['status']['watermark'] is not None
+            assert fields['status']['name'] is not None
+            assert fields['status']['not_none'] is not None
+            assert fields['status']['required'] is not None
+            assert fields['status']['default'] is not None
+
+            assert fields['dueDate']['label'] is not None
+            assert fields['dueDate']['watermark'] is not None
+            assert fields['dueDate']['name'] is not None
+            assert fields['dueDate']['not_none'] is not None
+            assert fields['dueDate']['required'] is not None
+            assert fields['dueDate']['pattern'] is not None
+            assert fields['dueDate']['example'] is not None
+
+            assert fields['kind']['label'] is not None
+            assert fields['kind']['watermark'] is not None
+            assert fields['kind']['name'] is not None
+            assert fields['kind']['not_none'] is not None
+            assert fields['kind']['required'] is not None
+
+            assert fields['days']['label'] is not None
+            assert fields['days']['watermark'] is not None
+            assert fields['days']['minimum'] is not None
+            assert fields['days']['not_none'] is not None
+            assert fields['days']['required'] is not None
+
