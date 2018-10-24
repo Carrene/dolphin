887f57e7a67249ae15dc340e6e9ae165c8fbb45b
shayan <shayan>
2018-10-24 12:54:55 +0330

    Add verb get to Member, closes #181

--- dolphin/controllers/members.py
+++ dolphin/controllers/members.py
@@ -1,4 +1,4 @@
-from nanohttp import json
+from nanohttp import json, HTTPNotFound
 from restfulpy.authorization import authorize
 from restfulpy.controllers import ModelRestController
 from restfulpy.orm import DBSession
@@ -15,4 +15,20 @@ class MemberController(ModelRestController):
     def list(self):
         query = DBSession.query(Member)
         return query
+
+    @authorize
+    @json(prevent_form='709 Form Not Allowed')
+    @Member.expose
+    def get(self, id):
+
+        try:
+            id = int(id)
+        except (ValueError, TypeError):
+            raise HTTPNotFound()
+
+        member = DBSession.query(Member).filter(Member.id == id).one_or_none()
+        if not member:
+            raise HTTPNotFound()
+
+        return member
 
---
+++ dolphin/tests/test_get_member.py
@@ -0,0 +1,55 @@
+from bddrest import status, response, when
+
+from dolphin.models import Member
+from dolphin.tests.helpers import LocalApplicationTestCase, oauth_mockup_server
+
+
+class TestProject(LocalApplicationTestCase):
+
+    @classmethod
+    def mockup(cls):
+        session = cls.create_session()
+
+        member1 = Member(
+            title='First Member',
+            email='member1@example.com',
+            access_token='access token 1',
+            phone=123456789,
+            reference_id=2
+        )
+        session.add(member1)
+        session.commit()
+
+    def test_get(self):
+        self.login('member1@example.com')
+
+        with oauth_mockup_server(), self.given(
+            'Getting a project',
+            '/apiv1/members/id:1',
+            'GET'
+        ):
+            assert status == 200
+            assert response.json['id'] == 1
+            assert response.json['title'] == 'member1'
+
+            when(
+                'Intended project with string type not found',
+                url_parameters=dict(id='Alphabetical')
+            )
+            assert status == 404
+
+            when(
+                'Intended project with string type not found',
+                url_parameters=dict(id=100)
+            )
+            assert status == 404
+
+            when(
+                'Form parameter is sent with request',
+                form=dict(parameter='Invalid form parameter')
+            )
+            assert status == 709
+
+            when('Request is not authorized', authorization=None)
+            assert status == 401
+
