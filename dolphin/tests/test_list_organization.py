5e91922e0516df60c14fc9e57c8bef0a8a862c5d
Mohammad Sheikhian <39209592+mohammadsheikhian@users.noreply.github.com>
2018-12-10 10:42:05 +0330

    Enhancement the list method of the organization controller (#275)
    * Enhancement the list method for the organization controller, closes #274
    
    * The coding style is edited

--- dolphin/tests/test_list_organization.py
+++ dolphin/tests/test_list_organization.py
@@ -1,7 +1,7 @@
 from bddrest.authoring import when, status, response
 
 from dolphin.models import Member, Organization, OrganizationMember
-from dolphin.tests.helpers import LocalApplicationTestCase
+from dolphin.tests.helpers import LocalApplicationTestCase, oauth_mockup_server
 
 
 class TestOrganization(LocalApplicationTestCase):
@@ -62,7 +62,7 @@ class TestOrganization(LocalApplicationTestCase):
 
         session.commit()
 
-    def test_list(self):
+    def test_unauthorize_list(self):
         with self.given(
             'List of organization',
             '/apiv1/organizations',
@@ -96,4 +96,51 @@ class TestOrganization(LocalApplicationTestCase):
             when('Trying filtering response', query=dict(id=1))
             assert response.json[0]['id'] == 1
             assert len(response.json) == 1
+
+    def test_authoriz_list(self):
+        self.login(email=self.owner1.email)
+
+        with oauth_mockup_server(), self.given(
+            f'List of organization',
+            f'/apiv1/members/id: {self.owner1.id}/organizations',
+            f'LIST',
+        ):
+            assert status == 200
+            assert len(response.json) == 2
+
+            when(
+                'Trying to pass with wrong member id',
+                url_parameters=dict(id=self.member1.id)
+            )
+            assert status == 404
+
+            when('Trying to pass with wrong id', url_parameters=dict(id=0))
+            assert status == 404
+
+            when('Type of id is invalid', url_parameters=dict(id='not-integer'))
+            assert status == 404
+
+            when('Trying to sorting response', query=dict(sort='id'))
+            assert response.json[0]['id'] == 1
+            assert response.json[1]['id'] == 2
+
+            when('Sorting the response descending', query=dict(sort='-id'))
+            assert response.json[0]['id'] == 2
+            assert response.json[1]['id'] == 1
+
+            when('Trying pagination response', query=dict(take=1))
+            assert response.json[0]['id'] == 1
+            assert len(response.json) == 1
+
+            when('Trying pagination with skip', query=dict(take=1, skip=1))
+            assert response.json[0]['id'] == 2
+            assert len(response.json) == 1
+
+            when('Trying filtering response', query=dict(id=1))
+            assert response.json[0]['id'] == 1
+            assert len(response.json) == 1
+
+            self.logout()
+            when('Request is not authorized', authorization=None)
+            assert status == 401
 
