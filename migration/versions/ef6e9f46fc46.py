a99cfb322e504d0beeb90e60c753117f9d79f4d1
shayan <shayan>
2018-12-17 17:33:07 +0330

    add organization_member

--- migration/versions/ef6e9f46fc46.py
+++ migration/versions/ef6e9f46fc46.py
@@ -7,7 +7,7 @@ Create Date: 2018-12-17 14:42:33.449580
 """
 from alembic import op
 import sqlalchemy as sa
-from dolphin.models.organization import Logo
+from dolphin.models.organization import Logo, roles
 
 
 # revision identifiers, used by Alembic.
@@ -19,12 +19,23 @@ depends_on = None
 
 def upgrade():
     op.create_table(
-        'organization',
-        sa.Column('title', sa.Unicode(50), index=True),
-        sa.Column('url', sa.Unicode(50), nullable=True),
-        sa.Column('domain', sa.Unicode(50), nullable=True),
-        sa.Column('_logo', Logo.as_mutable(sa.JSON), nullable=True)
+        'organization_member',
+        sa.Column(
+            'member_id',
+            sa.Integer,
+            sa.ForeignKey('member.id'),
+            primary_key=True
+        ),
+        sa.Column(
+            'organization_id',
+            sa.Integer,
+            sa.ForeignKey('organization.id'),
+            primary_key=True
+        ),
+        sa.Column('role', sa.Enum(*roles, name='roles'))
     )
+
 
 def downgrade():
     pass
+
