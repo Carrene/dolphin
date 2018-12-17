f92f3bb2f5ace2ce9562faf50c9d26329f5210b4
shayan <shayan>
2018-12-17 19:12:03 +0330

    Organization migration

--- migration/versions/ef6e9f46fc46.py
+++ migration/versions/ef6e9f46fc46.py
@@ -1,8 +1,8 @@
-"""add organization
+"""Add name to member
 
 Revision ID: ef6e9f46fc46
-Revises: 31f51bad2b24
-Create Date: 2018-12-17 14:42:33.449580
+Revises:
+Create Date: 2018-11-26 20:25:49.834678
 
 """
 from alembic import op
@@ -19,6 +19,13 @@ depends_on = None
 
 def upgrade():
     op.create_table(
+        'organization',
+        sa.Column('title', sa.Unicode(50), index=True),
+        sa.Column('url', sa.Unicode(50), nullable=True),
+        sa.Column('domain', sa.Unicode(50), nullable=True),
+        sa.Column('_logo', Logo.as_mutable(sa.JSON), nullable=True)
+    )
+    op.create_table(
         'organization_member',
         sa.Column(
             'member_id',
@@ -33,6 +40,23 @@ def upgrade():
             primary_key=True
         ),
         sa.Column('role', sa.Enum(*roles, name='roles'))
+    )
+    op.create_table(
+        'invitation',
+        sa.Column(
+            'by_member_id',
+            sa.Integer,
+            sa.ForeignKey('member.id'),
+        ),
+        sa.Column(
+            'organization_id',
+            sa.Integer,
+            sa.ForeignKey('organization.id'),
+        ),
+        sa.Column('role', sa.Enum(*roles, name='roles')),
+        sa.Column('accepted', sa.Boolean, default=False),
+        sa.Column('expired_date', sa.DateTime, nullable=False),
+        sa.Column('email', sa.Unicode(100), unique=True, index=True)
     )
 
 
