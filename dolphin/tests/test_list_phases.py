759c6d9f38770dbcdf86b7a2be0b8a060bcaa635
mkhfring <khajezade.mohamad@gmail.com>
2018-12-18 17:42:48 +0330

    List the phases API is implemented

--- dolphin/tests/test_list_phases.py
+++ dolphin/tests/test_list_phases.py
@@ -1,6 +1,6 @@
 from bddrest.authoring import status, response
 
-from dolphin.models import Phase, Workflow
+from dolphin.models import Phase, Workflow, Member
 from dolphin.tests.helpers import LocalApplicationTestCase, oauth_mockup_server
 
 
@@ -9,6 +9,14 @@ class TestListPhase(LocalApplicationTestCase):
     @classmethod
     def mockup(cls):
         cls.session = cls.create_session()
+        member = Member(
+            title='First Member',
+            email='member1@example.com',
+            access_token='access token 1',
+            phone=123456789,
+            reference_id=2
+        )
+        cls.session.add(member)
         triage = Phase(title='triage', order=0)
         design = Phase(title='design', order=2)
         implement = Phase(title='implement', order=3)
@@ -20,11 +28,13 @@ class TestListPhase(LocalApplicationTestCase):
         cls.session.commit()
 
     def test_list_phases(self):
+        self.login('member1@example.com')
+
         with oauth_mockup_server(), self.given(
             'List phases of a workflow',
             '/apiv1/workflows/id:1/phases',
             'LIST',
         ):
-            from pudb import set_trace; set_trace()
             assert status == 200
+            assert len(response.json) == 2
 
