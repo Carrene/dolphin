7b1cc766d85859d5e002e6d5b8c87e83600a29f4
mohammad <mohammadsheikhian70@gmail.com>
2018-12-22 17:14:29 +0330

    Implementing the move method for the issue controller, closes #306

--- dolphin/controllers/issues.py
+++ dolphin/controllers/issues.py
@@ -8,7 +8,7 @@ from ..exceptions import RoomMemberAlreadyExist, RoomMemberNotFound, \
     ChatRoomNotFound
 from ..models import Issue, Subscription, Resource, Phase, Item, Member
 from ..validators import issue_validator, update_issue_validator, \
-    assign_issue_validator
+    assign_issue_validator, issue_move_validator
 
 
 class IssueController(ModelRestController):
@@ -299,5 +299,24 @@ class IssueController(ModelRestController):
         if not issue:
             raise HTTPNotFound()
 
+        return issue
+
+    @authorize
+    @json(prevent_empty_form='708 No Parameter Exists In The Form')
+    @issue_move_validator
+    @commit
+    def move(self, id):
+        try:
+            id = int(id)
+
+        except (ValueError, TypeError):
+            raise HTTPNotFound()
+
+        issue = DBSession.query(Issue).filter(Issue.id == id).one_or_none()
+        if not issue:
+            raise HTTPNotFound()
+
+        issue.project_id = context.form.get('projectId')
+        DBSession.add(issue)
         return issue
 
