5b3cb71701c34fbd8794b5be0e589d1c9cca8106
Mohammad Sheikhian <39209592+mohammadsheikhian@users.noreply.github.com>
2019-01-14 16:49:53 +0330

    Adding the audit log middleware (#378)
    * Adding the audit log middleware to the wsgi
    
    * Adding a middleware for audit log
    
    * Creating a new mixin for audit log
    
    * The coding style is edited

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,15 @@
+from auditing import MiddleWareFactory
+
 from dolphin import dolphin as app
+
+
+def callback(audit_log):
+    print(f'verb: {audit_log[0].verb}')
+    print(f'status: {audit_log[0].status}')
 
 
 app.configure()
 app.initialize_orm()
+middleware = MiddleWareFactory(callback)
+app = middleware(app)
 
