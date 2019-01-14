e7b938d249e57fce8cf90c6fc8abd4ab36d4e0c4
ArashFatahzade <fatahzade@gmail.com>
2019-01-14 14:53:24 +0330

    Added GET Group API

--- dolphin/controllers/group.py
+++ dolphin/controllers/group.py
@@ -1,4 +1,4 @@
-from nanohttp import json
+from nanohttp import json, HTTPNotFound, int_or_notfound
 from restfulpy.authorization import authorize
 from restfulpy.controllers import ModelRestController
 from restfulpy.orm import DBSession
@@ -14,3 +14,16 @@ class GroupController(ModelRestController):
     @Group.expose
     def list(self):
         return DBSession.query(Group)
+
+
+    @authorize
+    @json(prevent_form='709 Form Not Allowed')
+    @Group.expose
+    def get(self, id):
+        id = int_or_notfound(id)
+        group = DBSession.query(Group).get(id)
+
+        if not group:
+            raise HTTPNotFound()
+
+        return group
