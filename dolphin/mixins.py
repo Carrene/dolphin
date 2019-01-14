785fd2ab6b40888b45292eb80cefb10333c36a0f
mohammad <mohammadsheikhian70@gmail.com>
2019-01-14 14:58:06 +0330

    Creating a new mixin for audit log

---
+++ dolphin/mixins.py
@@ -0,0 +1,28 @@
+from restfulpy.orm import DBSession
+from restfulpy.utils import to_camel_case
+from sqlalchemy import event, inspect
+
+
+class AuditLogMixin:
+
+    def __setattr__(self, attribute, value):
+
+        if attribute != '_sa_instance_state':
+            mapper_info = inspect(self)
+
+            if not mapper_info.pending and \
+                self in DBSession and \
+                to_camel_case(attribute) in self.json_metadata()['fields'] and \
+                value != getattr(self, attribute):
+
+                    print(f'Setting Attribute: {attribute} with value: '
+                          f'{value}, Old: {getattr(self, attribute)} related '
+                          f'to {type(self).__name__} with id: {self.id}')
+
+        super().__setattr__(attribute, value)
+
+
+@event.listens_for(AuditLogMixin, 'after_insert', propagate=True)
+def after_insert(mapper, connection, target):
+    print(f'Creating the {type(target).__name__} with id: {target.id}')
+
