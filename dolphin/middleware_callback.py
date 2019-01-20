5a73e720446d6ced86f5ef73e3fa1dfc41969928
mohammad <mohammadsheikhian70@gmail.com>
2019-01-20 17:37:12 +0330

    Creating new module for the audit log callback

---
+++ dolphin/middleware_callback.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+from auditing.logentry import ChangeAttributeLogEntry, InstantiationLogEntry
+from nanohttp import context
+from restfulpy.datetimehelpers import format_datetime
+
+from dolphin.backends import ChatClient
+from dolphin.models import Member
+
+
+AUDIT_LOG_MIMETYPE = 'application/x-auditlog'
+
+
+def callback(audit_log):
+
+    if audit_log[-1].status == '200 OK' and len(audit_log) > 1:
+        chat_client = ChatClient()
+        member = Member.current()
+        # FIXME: We will rollback if cannot send a message successfully
+        for log in audit_log:
+            if isinstance(log, ChangeAttributeLogEntry):
+                message = dict(
+                    action='Update',
+                    attribute=log.attribute,
+                    old=format_datetime(log.old_value) \
+                        if isinstance(log.old_value, datetime) else log.old_value,
+                    new=format_datetime(log.new_value) \
+                        if isinstance(log.new_value, datetime) else log.new_value,
+                )
+                chat_client.send_message(
+                    room_id=log.obj.room_id,
+                    body=message,
+                    mimetype=AUDIT_LOG_MIMETYPE,
+                    token=context.environ['HTTP_AUTHORIZATION'],
+                    x_access_token=member.access_token,
+                )
+
+            elif isinstance(log, InstantiationLogEntry):
+                message = dict(
+                    action='Create',
+                    attribute=None,
+                    old=None,
+                    new=None,
+                )
+                chat_client.send_message(
+                    room_id=log.obj.room_id,
+                    body=message,
+                    mimetype=AUDIT_LOG_MIMETYPE,
+                    token=context.environ['HTTP_AUTHORIZATION'],
+                    x_access_token=member.access_token,
+                )
+
