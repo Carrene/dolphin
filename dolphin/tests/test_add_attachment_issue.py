064eb53a31caa42ab3b9236f5a5b0d9b0f799a61
shayan-7 <39003982+shayan-7@users.noreply.github.com>
2019-02-03 12:59:13 +0330

    Add attachment to issue (#454)
    * Add attachment to issue
    
    * Little enahncement
    
    * Coding style

---
+++ dolphin/tests/test_add_attachment_issue.py
@@ -0,0 +1,147 @@
+import io
+from os.path import join, dirname, abspath
+
+from auditing.context import Context as AuditLogContext
+from bddrest import status, response, when, Update, Remove
+
+from dolphin.models import Project, Member, Workflow, Group, Release, Issue
+from dolphin.tests.helpers import LocalApplicationTestCase, oauth_mockup_server
+
+
+this_dir = abspath(join(dirname(__file__)))
+image_path = join(this_dir, 'stuff', '150x150.png')
+maximum_image_path = join(this_dir, 'stuff', 'maximum-length.jpg')
+
+
+class TestIssue(LocalApplicationTestCase):
+
+    @classmethod
+    @AuditLogContext(dict())
+    def mockup(cls):
+        session = cls.create_session()
+
+        member1 = Member(
+            title='First Member',
+            email='member1@example.com',
+            access_token='access token 1',
+            phone=123456789,
+            reference_id=2
+        )
+
+        workflow = Workflow(title='Default')
+        group = Group(title='default')
+
+        release = Release(
+            title='My first release',
+            description='A decription for my first release',
+            cutoff='2030-2-20',
+        )
+
+        cls.project = Project(
+            release=release,
+            workflow=workflow,
+            group=group,
+            member=member1,
+            title='My first project',
+            description='A decription for my project',
+            room_id=1001
+        )
+        session.add(cls.project)
+
+        cls.issue = Issue(
+            project=cls.project,
+            title='First issue',
+            description='This is description of first issue',
+            due_date='2020-2-20',
+            kind='feature',
+            days=1,
+            room_id=2
+        )
+        session.add(cls.issue)
+        session.commit()
+
+    def test_add_attachment(self):
+        self.login('member1@example.com')
+
+        with oauth_mockup_server(), open(image_path, 'rb') as f, self.given(
+            'add an attachment to a project',
+            f'/apiv1/issues/id:{self.issue.id}/files',
+            'ATTACH',
+            multipart=dict(
+                title='example',
+                attachment=io.BytesIO(f.read())
+            )
+        ):
+            assert status == 200
+            assert response.json['title'] == 'example'
+            assert response.json['id'] is not None
+            assert response.json['createdAt'] is not None
+            assert 'isMine' in response.json
+
+            with open(maximum_image_path, 'rb') as f:
+                when(
+                    'Image size is more than maximum length',
+                    multipart=Update(
+                        caption='This is caption',
+                        attachment=io.BytesIO(f.read())
+                    )
+                )
+                assert status == 413
+
+            when(
+                'Try to pass without sending the file',
+                multipart=Remove('attachment')
+            )
+            assert status == '758 File Not In Form'
+
+            when(
+                'Title is more than 50 charecters',
+                multipart=Update(title=(50 + 1) * 'a')
+            )
+            assert status == '704 At Most 50 Characters Are Valid For Title'
+
+            with open(maximum_image_path, 'rb') as f:
+                when(
+                    'Image size is more than maximum length',
+                    multipart=Update(
+                        attachment=io.BytesIO(f.read())
+                    )
+                )
+                assert status == 413
+
+            when(
+                'Try to pass without sending the file',
+                multipart=Remove('attachment')
+            )
+            assert status == '758 File Not In Form'
+
+            when(
+                'Title is more than 50 charecters',
+                multipart=Update(title=(50 + 1) * 'a')
+            )
+            assert status == '704 At Most 50 Characters Are Valid For Title'
+
+            with open(maximum_image_path, 'rb') as f:
+                when(
+                    'Image size is more than maximum length',
+                    multipart=Update(
+                        attachment=io.BytesIO(f.read())
+                    )
+                )
+                assert status == 413
+
+            when(
+                'Try to pass without sending the file',
+                multipart=Remove('attachment')
+            )
+            assert status == '758 File Not In Form'
+
+            when(
+                'Title is more than 50 charecters',
+                multipart=Update(title=(50 + 1) * 'a')
+            )
+            assert status == '704 At Most 50 Characters Are Valid For Title'
+
+            when('Request is not authorized', authorization=None)
+            assert status == 401
+
