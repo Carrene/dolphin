65e909d0269a611956fc28285c5021a845626e80
mohammad <mohammadsheikhian70@gmail.com>
2019-02-18 12:18:58 +0330

    Renamin the auditing to auditor

--- dolphin/mixins.py
+++ dolphin/mixins.py
@@ -1,4 +1,4 @@
-from auditort import context as AuditLogContext
+from auditor import context as AuditLogContext
 from nanohttp import context
 from restfulpy.orm import DBSession
 from restfulpy.utils import to_camel_case
