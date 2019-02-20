1d2df4a4abcea6df904d233ac9793e0e536c6e11
shayan <shayan>
2019-02-20 17:22:56 +0330

    Fix a bug on subscribing issue and project

--- dolphin/tests/test_subscribe_issue.py
+++ dolphin/tests/test_subscribe_issue.py
@@ -18,7 +18,7 @@ class TestIssue(LocalApplicationTestCase):
             email='member1@example.com',
             access_token='access token 1',
             phone=123456789,
-            reference_id=1
+            reference_id=2
         )
 
         workflow = Workflow(title='Default')
