aba21b54bb412b5d92ab35e36dd080748403bec4
shayan <shayan>
2019-04-06 17:49:34 +0430

    Enhance tests

--- dolphin/tests/test_update_issue.py
+++ dolphin/tests/test_update_issue.py
@@ -189,7 +189,7 @@ class TestIssue(LocalApplicationTestCase):
                     dict(title='Another title')
             )
             assert status == '705 Invalid status, only one of "to-do, ' \
-                'in-progress, done, complete, on-hold" will be accepted'
+                'in-progress, on-hold, complete, done" will be accepted'
             assert status.text.startswith('Invalid status')
 
             when(
