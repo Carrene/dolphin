ed00f8bb3f497614ac2dcbc3b3c977fa77d980ac
mohammad <mohammadsheikhian70@gmail.com>
2019-04-08 10:44:25 +0430

    The coding style is modified

--- dolphin/tests/test_create_workflow.py
+++ dolphin/tests/test_create_workflow.py
@@ -77,7 +77,7 @@ class TestWorkflow(LocalApplicationTestCase):
                 'Description length is less than limit',
                 json=Update(description=((8192 + 1) * 'a')),
             )
-            assert status == '703 At Most 8192 Characters Are Valid For '\
+            assert status == '703 At Most 8192 Characters Are Valid For ' \
                 'Description'
 
             when('Request is not authorized', authorization=None)
