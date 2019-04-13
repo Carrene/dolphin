d376e8078c1033b5286d360434ce479598f66a3d
mahdinazari <41826698+mahdinazari@users.noreply.github.com>
2019-04-13 10:51:05 +0430

    Implemented the get method on the phase controller, closes #669 (#669)

---
+++ dolphin/tests/test_phase_get.py
@@ -0,0 +1,69 @@
+from bddrest import status, response, when, given
+from dolphin.models import Member, Skill, Phase, Workflow
+from dolphin.tests.helpers import LocalApplicationTestCase, oauth_mockup_server
+
+
+class TestPhase(LocalApplicationTestCase):
+
+    @classmethod
+    def mockup(cls):
+        session = cls.create_session()
+        cls.member = Member(
+            title='First Member',
+            email='member@example.com',
+            access_token='access token 1',
+            phone=123456789,
+            reference_id=1,
+        )
+        session.add(cls.member)
+
+        skill1 = Skill(title='skill 1')
+
+        cls.workflow = Workflow(
+            title='workflow',
+        )
+
+        cls.phase = Phase(
+             title='phase 1',
+             order=1,
+             skill=skill1,
+             workflow=cls.workflow,
+        )
+        session.add(cls.phase)
+        session.commit()
+
+    def test_get(self):
+        self.login(self.member.email)
+        with oauth_mockup_server(), self.given(
+            f'Getting a phase',
+            f'/apiv1/workflows/workflow_id: {self.workflow.id}' \
+               f'/phases/phase_id: {self.phase.id}',
+            f'GET',
+        ):
+            assert status == 200
+            assert response.json['id'] == self.phase.id
+
+            when(
+                'Phase Not Found',
+                url_parameters=given | dict(phase_id=0)
+            )
+            assert status == 404
+
+            when(
+                'Phase Not Found',
+                url_parameters=given | dict(phase_id='not-integer')
+            )
+            assert status == 404
+
+            when(
+                'Form Parameter',
+                form=dict(a='a')
+            )
+            assert status == '709 Form Not Allowed'
+
+            when(
+                'Request is not authorized',
+                authorization=None,
+            )
+            assert status == 401
+
