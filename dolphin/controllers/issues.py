31f71895faa5fd3d93497f9d0b45b098d6eb805d
shayan <shayan>
2019-04-22 17:57:24 +0430

    Enhancement on LIST Issue

--- dolphin/controllers/issues.py
+++ dolphin/controllers/issues.py
@@ -237,7 +237,7 @@ class IssueController(ModelRestController, JsonPatchControllerMixin):
 
                 query = Issue._sort_by_key_value(
                     query,
-                    column=item_cte.c.max_item_phase_id,
+                    column=Item.phase_id,
                     descending=sorting_columns['phaseId']
                 )
 
@@ -258,7 +258,7 @@ class IssueController(ModelRestController, JsonPatchControllerMixin):
                 if not 'phaseTitle' in context.query:
                     query = query.join(
                         Phase,
-                        Phase.id == item_cte.c.max_item_phase_id,
+                        Phase.id == Item.phase_id,
                         isouter=True
                     )
 
