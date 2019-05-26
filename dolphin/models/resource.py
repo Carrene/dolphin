4c7470880cf4296dc62cde40f33a3f0f56f6f658
shayan <shayan>
2019-05-26 17:12:16 +0430

    Add rest of resource model tests

--- dolphin/models/resource.py
+++ dolphin/models/resource.py
@@ -1,6 +1,7 @@
 from restfulpy.orm import DeclarativeBase, Field, relationship
-from sqlalchemy import Integer, ForeignKey, select, func, join
+from sqlalchemy import Integer, ForeignKey, select, func, join, case, and_
 from sqlalchemy.orm import column_property
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from .member import Member
 from .issue import Issue
@@ -38,11 +39,34 @@ class Resource(Member):
         protected=True
     )
 
-    load = column_property(
+    load_value = column_property(
         select([func.sum(Issue.priority_value)]) \
         .select_from(
             join(Item, Issue, Item.issue_id == Issue.id),
         ) \
-        .where(Item.member_id == 1)
+        .where(Item.member_id == Member.id)
     )
+
+    @hybrid_property
+    def load(self):
+        if self.load_value is None:
+            return None
+
+        if 5 < self.load_value:
+            return 'heavy'
+
+        elif 3 <= self.load_value <= 5:
+            return 'medium'
+
+        elif 3 > self.load_value:
+            return 'light'
+
+    @load.expression
+    def load(cls):
+        return case([
+            (None == cls.load_value, None),
+            (5 < cls.load_value, 'heavy'),
+            (and_(3 <= cls.load_value,  cls.load_value <= 5), 'medium'),#<= 5
+            (3 > cls.load_value, 'low')
+        ])
 
