f06c4969b7692ea95cafefa99726b0d42c986d9b
shayan <shayan>
2019-05-26 12:35:01 +0430

    Fix a bug on response_time value

--- dolphin/models/item.py
+++ dolphin/models/item.py
@@ -178,8 +178,12 @@ class Item(TimestampMixin, OrderingMixin, FilteringMixin, PaginationMixin,
 
     def to_dict(self):
         item_dict = super().to_dict()
+        item_dict['responseTime'] = \
+            self._get_hours_from_timedelta(self.response_time) \
+            if self.response_time else None
+
         item_dict['hoursWorked'] = self.hours_worked
-        item_dict['responseTime'] = self.response_time
+        item_dict['perspective'] = self.perspective
         return item_dict
 
     @classmethod
@@ -205,4 +209,8 @@ class Item(TimestampMixin, OrderingMixin, FilteringMixin, PaginationMixin,
             example='Lorem Ipsum',
             message='Lorem Ipsun',
         )
+
+    @staticmethod
+    def _get_hours_from_timedelta( timedelta):
+        return timedelta.seconds // 3600
 
