f1b74b45231e3e8510455bc72d7af8e16e69d16d
mohammad <mohammadsheikhian70@gmail.com>
2019-06-17 15:07:49 +0430

    Modified the migration

--- dolphin/migration/versions/5f206088190d_.py
+++ dolphin/migration/versions/5f206088190d_.py
@@ -12,6 +12,201 @@ from sqlalchemy import orm
 
 from dolphin.models import Item, Issue, IssuePhase
 
+from datetime import datetime, timedelta
+
+from nanohttp import settings
+from restfulpy.orm import Field, DeclarativeBase, relationship
+from restfulpy.orm.metadata import MetadataField
+from restfulpy.orm.mixins import TimestampMixin, OrderingMixin, \
+    FilteringMixin, PaginationMixin
+from sqlalchemy import Integer, ForeignKey, UniqueConstraint, DateTime, Enum, \
+    String, select, func
+from sqlalchemy.orm import column_property, synonym
+
+from dolphin.models.dailyreport import Dailyreport
+
+
+item_statuses = [
+    'in-progress',
+    'done',
+]
+
+
+class OldItem(TimestampMixin, OrderingMixin, FilteringMixin, PaginationMixin,
+           DeclarativeBase):
+    __tablename__ = 'old_item'
+
+    id = Field(
+        Integer,
+        primary_key=True,
+        readonly=True,
+        not_none=True,
+        required=False,
+        label='ID',
+        minimum=1,
+        example=1,
+        protected=False,
+    )
+    start_date = Field(
+        DateTime,
+        python_type=datetime,
+        label='Start Date',
+        pattern=
+            r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])'
+            r'(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z)?)?$',
+        pattern_description='ISO format and format like "yyyy-mm-dd" is valid',
+        example='2018-02-02T1:12:12.000Z',
+        nullable=True,
+        not_none=False,
+        required=False,
+        readonly=False,
+    )
+    end_date = Field(
+        DateTime,
+        python_type=datetime,
+        label='Target Date',
+        pattern=
+            r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])'
+            r'(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z)?)?$',
+        pattern_description='ISO format and format like "yyyy-mm-dd" is valid',
+        example='2018-02-02T1:12:12.000Z',
+        nullable=True,
+        not_none=False,
+        required=False,
+        readonly=False,
+    )
+    estimated_hours = Field(
+        Integer,
+        python_type=int,
+        label='Estimated Hours',
+        watermark='Enter hours you estimate',
+        readonly=False,
+        nullable=True,
+        not_none=False,
+        required=False,
+    )
+    _status = Field(
+        Enum(*item_statuses, name='item_status'),
+        python_type=str,
+        default='in-progress',
+        label='Status',
+        watermark='Choose a status',
+        nullable=True,
+        required=False,
+        example='Lorem Ipsum'
+    )
+    description = Field(
+        String,
+        min_length=1,
+        max_length=512,
+        label='Description',
+        watermark='Enter the description',
+        not_none=False,
+        nullable=True,
+        required=False,
+        python_type=str,
+        example='Lorem Ipsum'
+    )
+    phase_id = Field(
+        Integer,
+        ForeignKey('phase.id'),
+        python_type=int,
+        nullable=False,
+        watermark='Choose a phase',
+        label='Phase',
+        not_none=True,
+        required=True,
+        example='Lorem Ipsum'
+    )
+    issue_id = Field(
+        Integer,
+        ForeignKey('issue.id'),
+        python_type=int,
+        nullable=False,
+        watermark='Choose an issue',
+        label='Issue',
+        not_none=True,
+        required=True,
+        example='Lorem Ipsum'
+    )
+    member_id = Field(
+        Integer,
+        ForeignKey('member.id'),
+        python_type=int,
+        nullable=False,
+        watermark='Choose a member',
+        label='Member',
+        not_none=True,
+        required=True,
+        example='Lorem Ipsum'
+    )
+    _last_status_change = Field(
+        DateTime,
+        python_type=datetime,
+        nullable=True,
+        protected=True,
+    )
+
+    hours_worked = column_property(
+        select([func.sum(Dailyreport.hours)])
+        .where(Dailyreport.item_id == id)
+    )
+
+    UniqueConstraint(phase_id, issue_id, member_id)
+
+    @property
+    def perspective(self):
+        if len(self.dailyreports) == 0:
+            return 'Due'
+
+        for dailyreport in self.dailyreports:
+            if dailyreport.note == None \
+                    and dailyreport.date < datetime.now().date():
+                return 'Overdue'
+
+        if self.dailyreports[-1].note == None \
+                and self.dailyreports[-1].date == datetime.now().date():
+            return 'Due'
+
+        return 'Submitted'
+
+    def _set_status(self, status):
+        if status == 'in-progress':
+            self._last_status_change = datetime.now()
+
+        self._status = status
+
+    def _get_status(self):
+        return self._status
+
+    status = synonym(
+        '_status',
+        descriptor=property(_get_status, _set_status),
+        info=dict(protected=True)
+    )
+
+    @property
+    def response_time(self):
+        if self.status == 'in-progress':
+          return (
+              (self._last_status_change or self.created_at) + \
+              timedelta(hours=settings.item.response_time)
+          ) - datetime.now()
+
+    def to_dict(self):
+        item_dict = super().to_dict()
+        item_dict['responseTime'] = \
+            self._get_hours_from_timedelta(self.response_time) \
+            if self.response_time else None
+
+        item_dict['hoursWorked'] = self.hours_worked
+        item_dict['perspective'] = self.perspective
+        return item_dict
+
+    @staticmethod
+    def _get_hours_from_timedelta( timedelta):
+        return timedelta.seconds / 3600
+
 
 # revision identifiers, used by Alembic.
 revision = '5f206088190d'
@@ -21,6 +216,7 @@ depends_on = None
 
 
 def upgrade():
+    import pudb; pudb.set_trace()  # XXX BREAKPOINT
     bind = op.get_bind()
     session = orm.Session(bind=bind)
 
@@ -45,8 +241,8 @@ def upgrade():
 
 
     op.add_column('item', sa.Column('issue_phase_id', sa.Integer(), nullable=True))
-    items = session.query(Item).all()
-    for item in items:
+    old_items = session.query(OldItem).all()
+    for item in old_items:
         try:
             issue_phase = IssuePhase(
                 issue_id=item.issue_id,
@@ -56,11 +252,17 @@ def upgrade():
             session.commit()
 
         except:
-            pass
-
-        item.issue_phase_id = issue_phase.id
+            issue_phase = session.query(IssuePhase) \
+                .filter(
+                    IssuePhase.issue_id == item.issue_id,
+                    IssuePhase.phase_id == item.phase_id
+                ) \
+                .one_or_none()
+        new_item = session.query(Item).get(item.id)
+        new_item.issue_phase_id = issue_phase.id
         session.commit()
 
+    import pudb; pudb.set_trace()  # XXX BREAKPOINT
     op.drop_constraint('item_phase_id_issue_id_member_id_key', 'item', type_='unique')
     op.drop_constraint('item_issue_id_fkey', 'item', type_='foreignkey')
     op.drop_constraint('item_phase_id_fkey', 'item', type_='foreignkey')
