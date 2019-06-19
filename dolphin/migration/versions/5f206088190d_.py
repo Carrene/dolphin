080f5ec25a571b723b609ec3810125e3a32e446a
mohammad <mohammadsheikhian70@gmail.com>
2019-06-19 12:32:03 +0430

    Modified the migration related to the old item

--- dolphin/migration/versions/5f206088190d_.py
+++ dolphin/migration/versions/5f206088190d_.py
@@ -27,158 +27,6 @@ item_statuses = [
 ]
 
 
-class OldItem(TimestampMixin, OrderingMixin, FilteringMixin, PaginationMixin,
-           DeclarativeBase):
-    __tablename__ = 'old_item'
-    __table_args__ = {'extend_existing': True}
-
-    id = Field(
-        Integer,
-        primary_key=True,
-        readonly=True,
-        not_none=True,
-        required=False,
-        label='ID',
-        minimum=1,
-        example=1,
-        protected=False,
-    )
-    start_date = Field(
-        DateTime,
-        python_type=datetime,
-        label='Start Date',
-        pattern=
-            r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])'
-            r'(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z)?)?$',
-        pattern_description='ISO format and format like "yyyy-mm-dd" is valid',
-        example='2018-02-02T1:12:12.000Z',
-        nullable=True,
-        not_none=False,
-        required=False,
-        readonly=False,
-    )
-    end_date = Field(
-        DateTime,
-        python_type=datetime,
-        label='Target Date',
-        pattern=
-            r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])'
-            r'(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z)?)?$',
-        pattern_description='ISO format and format like "yyyy-mm-dd" is valid',
-        example='2018-02-02T1:12:12.000Z',
-        nullable=True,
-        not_none=False,
-        required=False,
-        readonly=False,
-    )
-    estimated_hours = Field(
-        Integer,
-        python_type=int,
-        label='Estimated Hours',
-        watermark='Enter hours you estimate',
-        readonly=False,
-        nullable=True,
-        not_none=False,
-        required=False,
-    )
-    description = Field(
-        String,
-        min_length=1,
-        max_length=512,
-        label='Description',
-        watermark='Enter the description',
-        not_none=False,
-        nullable=True,
-        required=False,
-        python_type=str,
-        example='Lorem Ipsum'
-    )
-    phase_id = Field(
-        Integer,
-        ForeignKey('phase.id'),
-        python_type=int,
-        nullable=False,
-        watermark='Choose a phase',
-        label='Phase',
-        not_none=True,
-        required=True,
-        example='Lorem Ipsum'
-    )
-    issue_id = Field(
-        Integer,
-        ForeignKey('issue.id'),
-        python_type=int,
-        nullable=False,
-        watermark='Choose an issue',
-        label='Issue',
-        not_none=True,
-        required=True,
-        example='Lorem Ipsum'
-    )
-    member_id = Field(
-        Integer,
-        ForeignKey('member.id'),
-        python_type=int,
-        nullable=False,
-        watermark='Choose a member',
-        label='Member',
-        not_none=True,
-        required=True,
-        example='Lorem Ipsum'
-    )
-
-    @property
-    def perspective(self):
-        if len(self.dailyreports) == 0:
-            return 'Due'
-
-        for dailyreport in self.dailyreports:
-            if dailyreport.note == None \
-                    and dailyreport.date < datetime.now().date():
-                return 'Overdue'
-
-        if self.dailyreports[-1].note == None \
-                and self.dailyreports[-1].date == datetime.now().date():
-            return 'Due'
-
-        return 'Submitted'
-
-    def _set_status(self, status):
-        if status == 'in-progress':
-            self._last_status_change = datetime.now()
-
-        self._status = status
-
-    def _get_status(self):
-        return self._status
-
-    status = synonym(
-        '_status',
-        descriptor=property(_get_status, _set_status),
-        info=dict(protected=True)
-    )
-
-    @property
-    def response_time(self):
-        if self.status == 'in-progress':
-          return (
-              (self._last_status_change or self.created_at) + \
-              timedelta(hours=settings.item.response_time)
-          ) - datetime.now()
-
-    def to_dict(self):
-        item_dict = super().to_dict()
-        item_dict['responseTime'] = \
-            self._get_hours_from_timedelta(self.response_time) \
-            if self.response_time else None
-
-        item_dict['perspective'] = self.perspective
-        return item_dict
-
-    @staticmethod
-    def _get_hours_from_timedelta( timedelta):
-        return timedelta.seconds / 3600
-
 
 # revision identifiers, used by Alembic.
 revision = '5f206088190d'
@@ -188,6 +36,157 @@ depends_on = None
 
 
 def upgrade():
+
+    class OldItem(TimestampMixin, OrderingMixin, FilteringMixin, PaginationMixin,
+               DeclarativeBase):
+        __tablename__ = 'old_item'
+
+        id = Field(
+            Integer,
+            primary_key=True,
+            readonly=True,
+            not_none=True,
+            required=False,
+            label='ID',
+            minimum=1,
+            example=1,
+            protected=False,
+        )
+        start_date = Field(
+            DateTime,
+            python_type=datetime,
+            label='Start Date',
+            pattern=
+                r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])'
+                r'(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z)?)?$',
+            pattern_description='ISO format and format like "yyyy-mm-dd" is valid',
+            example='2018-02-02T1:12:12.000Z',
+            nullable=True,
+            not_none=False,
+            required=False,
+            readonly=False,
+        )
+        end_date = Field(
+            DateTime,
+            python_type=datetime,
+            label='Target Date',
+            pattern=
+                r'^(\d{4})-(0[1-9]|1[012]|[1-9])-(0[1-9]|[12]\d{1}|3[01]|[1-9])'
+                r'(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z)?)?$',
+            pattern_description='ISO format and format like "yyyy-mm-dd" is valid',
+            example='2018-02-02T1:12:12.000Z',
+            nullable=True,
+            not_none=False,
+            required=False,
+            readonly=False,
+        )
+        estimated_hours = Field(
+            Integer,
+            python_type=int,
+            label='Estimated Hours',
+            watermark='Enter hours you estimate',
+            readonly=False,
+            nullable=True,
+            not_none=False,
+            required=False,
+        )
+        description = Field(
+            String,
+            min_length=1,
+            max_length=512,
+            label='Description',
+            watermark='Enter the description',
+            not_none=False,
+            nullable=True,
+            required=False,
+            python_type=str,
+            example='Lorem Ipsum'
+        )
+        phase_id = Field(
+            Integer,
+            ForeignKey('phase.id'),
+            python_type=int,
+            nullable=False,
+            watermark='Choose a phase',
+            label='Phase',
+            not_none=True,
+            required=True,
+            example='Lorem Ipsum'
+        )
+        issue_id = Field(
+            Integer,
+            ForeignKey('issue.id'),
+            python_type=int,
+            nullable=False,
+            watermark='Choose an issue',
+            label='Issue',
+            not_none=True,
+            required=True,
+            example='Lorem Ipsum'
+        )
+        member_id = Field(
+            Integer,
+            ForeignKey('member.id'),
+            python_type=int,
+            nullable=False,
+            watermark='Choose a member',
+            label='Member',
+            not_none=True,
+            required=True,
+        )
+
+        @property
+        def perspective(self):
+            if len(self.dailyreports) == 0:
+                return 'Due'
+
+            for dailyreport in self.dailyreports:
+                if dailyreport.note == None \
+                        and dailyreport.date < datetime.now().date():
+                    return 'Overdue'
+
+            if self.dailyreports[-1].note == None \
+                    and self.dailyreports[-1].date == datetime.now().date():
+                return 'Due'
+
+            return 'Submitted'
+
+        def _set_status(self, status):
+            if status == 'in-progress':
+                self._last_status_change = datetime.now()
+
+            self._status = status
+
+        def _get_status(self):
+            return self._status
+
+        status = synonym(
+            '_status',
+            descriptor=property(_get_status, _set_status),
+            info=dict(protected=True)
+        )
+
+        @property
+        def response_time(self):
+            if self.status == 'in-progress':
+              return (
+                  (self._last_status_change or self.created_at) + \
+                  timedelta(hours=settings.item.response_time)
+              ) - datetime.now()
+
+        def to_dict(self):
+            item_dict = super().to_dict()
+            item_dict['responseTime'] = \
+                self._get_hours_from_timedelta(self.response_time) \
+                if self.response_time else None
+
+            item_dict['perspective'] = self.perspective
+            return item_dict
+
+        @staticmethod
+        def _get_hours_from_timedelta( timedelta):
+            return timedelta.seconds / 3600
+
     bind = op.get_bind()
     session = orm.Session(bind=bind)
 
