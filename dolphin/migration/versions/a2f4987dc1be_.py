89150afc049a037250a4127c9d119ea508fe8af6
Mohammad Sheikhian <mohammadsheikhian70@gmail.com>
2019-06-26 15:15:52 +0430

    Added the migration related to the created by mixin (#948)
    * Added the migration related to the created by mixin
    
    * Modified the coding style
    
    * Modified the coding style

---
+++ dolphin/migration/versions/a2f4987dc1be_.py
@@ -0,0 +1,65 @@
+"""Modified the created by mixin
+
+Revision ID: a2f4987dc1be
+Revises: 508a09c9f43a
+Create Date: 2019-06-26 14:31:26.661081
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import orm
+
+from dolphin.models.issue import Issue
+
+
+# revision identifiers, used by Alembic.
+revision = 'a2f4987dc1be'
+down_revision = '508a09c9f43a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+
+    op.add_column(
+        'issue',
+        sa.Column('created_by_member_id', sa.Integer(), nullable=True)
+    )
+    op.add_column(
+        'issue',
+        sa.Column('created_by_reference_id', sa.Integer(), nullable=True)
+    )
+    op.drop_column('issue', 'created_by')
+
+    issues = session.query(Issue)
+    for issue in issues:
+        issue.created_by_reference_id = 34
+        issue.created_by_member_id = 33
+
+    session.commit()
+
+    op.execute(
+        'ALTER TABLE issue ALTER COLUMN created_by_member_id SET NOT NULL'
+    )
+
+    op.execute(
+        'ALTER TABLE issue ALTER COLUMN created_by_reference_id SET NOT NULL'
+    )
+
+
+def downgrade():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+
+    op.add_column(
+        'issue',
+        sa.Column('created_by', sa.INTEGER(), nullable=True)
+    )
+    op.drop_column('issue', 'created_by_reference_id')
+    op.drop_column('issue', 'created_by_member_id')
+
+    op.execute('UPDATE issue SET created_by = 33;')
+    op.execute('ALTER TABLE issue ALTER COLUMN created_by SET NOT NULL')
+
