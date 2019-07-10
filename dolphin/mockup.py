86fdba43b3a6014d2f0f60b2e02ea28afaefd801
shayan <shayan>
2019-07-10 17:22:18 +0430

    Enhance mockup

--- dolphin/mockup.py
+++ dolphin/mockup.py
@@ -81,10 +81,124 @@ def insert(): # pragma: no cover
             role='member',
         )
         DBSession.add(organization_resource3)
+
+        resource4 = Resource(
+            id=5,
+            title='User_4',
+            email='user4@example.com',
+            reference_id=5,
+            access_token='access token 5',
+            skill_id=skill.id,
+        )
+        DBSession.add(resource4)
+        DBSession.flush()
+
+        organization_resource4 = OrganizationMember(
+            organization_id=organization.id,
+            member_id=resource4.id,
+            role='member',
+        )
+        DBSession.add(organization_resource4)
+
+        resource5 = Resource(
+            id=6,
+            title='User_5',
+            email='user5@example.com',
+            reference_id=6,
+            access_token='access token 6',
+            skill_id=skill.id,
+        )
+        DBSession.add(resource5)
+        DBSession.flush()
+
+        organization_resource5 = OrganizationMember(
+            organization_id=organization.id,
+            member_id=resource5.id,
+            role='member',
+        )
+        DBSession.add(organization_resource5)
+
+        resource6 = Resource(
+            id=7,
+            title='User_6',
+            email='user6@example.com',
+            reference_id=7,
+            access_token='access token 7',
+            skill_id=skill.id,
+        )
+        DBSession.add(resource6)
+        DBSession.flush()
+
+        organization_resource6 = OrganizationMember(
+            organization_id=organization.id,
+            member_id=resource6.id,
+            role='member',
+        )
+        DBSession.add(organization_resource6)
+
+        resource7 = Resource(
+            id=8,
+            title='User_7',
+            email='user7@example.com',
+            reference_id=8,
+            access_token='access token 8',
+            skill_id=skill.id,
+        )
+        DBSession.add(resource7)
+        DBSession.flush()
+
+        organization_resource7 = OrganizationMember(
+            organization_id=organization.id,
+            member_id=resource7.id,
+            role='member',
+        )
+        DBSession.add(organization_resource7)
+
+        resource8 = Resource(
+            id=9,
+            title='User_8',
+            email='user8@example.com',
+            reference_id=9,
+            access_token='access token 8',
+            skill_id=skill.id,
+        )
+        DBSession.add(resource8)
+        DBSession.flush()
+
+        organization_resource8 = OrganizationMember(
+            organization_id=organization.id,
+            member_id=resource8.id,
+            role='member',
+        )
+        DBSession.add(organization_resource8)
+
+        resource9 = Resource(
+            id=10,
+            title='User_9',
+            email='user9@example.com',
+            reference_id=10,
+            access_token='access token 10',
+            skill_id=skill.id,
+        )
+        DBSession.add(resource9)
+        DBSession.flush()
+
+        organization_resource9 = OrganizationMember(
+            organization_id=organization.id,
+            member_id=resource9.id,
+            role='member',
+        )
+        DBSession.add(organization_resource9)
         DBSession.commit()
 
         print('Following resource has been added:')
         print(resource1)
         print(resource2)
         print(resource3)
+        print(resource4)
+        print(resource5)
+        print(resource6)
+        print(resource7)
+        print(resource8)
+        print(resource9)
 
