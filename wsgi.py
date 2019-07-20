6dac974616deb99af29ec22c01b5332374d17838
shayan <shayan>
2019-07-20 11:08:12 +0430

    Enhance wsgi

--- wsgi.py
+++ wsgi.py
@@ -5,11 +5,11 @@ from auditor import MiddleWare
 from dolphin import dolphin as app
 from dolphin.middleware_callback import callback
 
-#home_directory = os.environ['HOME']
-#configuration_file_name=f'{home_directory}/.config/dolphin.yml'
+home_directory = os.environ['HOME']
+configuration_file_name=f'{home_directory}/.config/dolphin.yml'
 
 
-app.configure()
+app.configure(filename=configuration_file_name)
 app.initialize_orm()
 app = MiddleWare(app, callback)
 
